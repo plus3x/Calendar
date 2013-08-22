6d0207db6cc986ecaba3ce1d894693bba8af88eb
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 15:25:18 +0400

    Reconfigure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TestForCalendar::Application.routes.draw do
-  root 'home#index'
+  root 'home#index', as: 'home'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
