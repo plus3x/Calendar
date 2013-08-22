5225323d1eb297dd753356da63ac863391479f95
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 04:21:19 +0400

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestForCalendar::Application.config.secret_key_base = '97d66a9aa89c9700a81590ac1bd0de22d8ae7b268a1e00b359670ec2908c8139b974e77c91e38cbcc5f41f018c139409ee5f5ece655fdd1bab8baf0e66d2c00b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestForCalendar::Application.config.session_store :cookie_store, key: '_test_for_calendar_session'
