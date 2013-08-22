7766216d4b5d196a4cc26528684c47189adc8d1a
miralas <miralas777@yandex.ru>
2013-08-22 17:41:50 +0300

    Delete Model Events
    rails destroy model event

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822122825) do
-
-  create_table "events", force: true do |t|
-    t.string   "title"
-    t.string   "name"
-    t.datetime "start_time"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
