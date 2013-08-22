d98f22c44643dd8e8b6b9495c99e65ae095f2e09
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 17:40:40 +0400

    Reconfigure Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,23 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: [:new, :create]
   
+  # GET /login
   def new
+  end
+
+  # POST /login
+  def create
     user = User.find_by(email: params[:email])
     if user and user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect_to admin_url
+      session[:user_id] = user.id
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
   end
 
-  def create
-  end
-
+  # DELETE /logout
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Logged out"
+    redirect_to home_url, notice: "Logged out"
   end
 end
