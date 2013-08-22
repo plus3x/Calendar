760e7e548ed9909a7dbff5785d098a1d62ca4244
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 16:36:34 +0400

    Set session create user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def new
+    user = User.find_by(email: params[:email])
+    if user and user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid user/password combination"
+    end
   end
 
   def create
