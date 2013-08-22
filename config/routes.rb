3c2ac533dfb56fc3152750f09e396570aca06ddf
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 16:41:52 +0400

    Set routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 TestForCalendar::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
+  
   resources :users
+  
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   root 'home#index', as: 'home'
   
