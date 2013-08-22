5872ed464ebfb9f53c8fe0508a7a75bc1bc89501
Vladislav Petrov <electronicchest@gmail.com>
2013-08-22 16:33:00 +0400

    Migrate
    rske db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822002405) do
+ActiveRecord::Schema.define(version: 20130822122825) do
 
   create_table "events", force: true do |t|
     t.string   "title"
     t.string   "name"
     t.datetime "start_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
